d9f8c4cc04d9660246c029723c426a24d496c150
RainyYC <1072874017@qq.com>
2026-03-14 17:57:23 +1300

    fix: Remove duplicated items in creative mode tab (#54)
    (cherry picked from commit e3b98e918b154d9d05a27b664f35bd27538fb6de)

--- src/main/kotlin/gregtechlite/gtlitecore/common/metatileentity/part/PartMachineAdvancedLaserHatch.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/common/metatileentity/part/PartMachineAdvancedLaserHatch.kt
@@ -4,65 +4,9 @@ import gregtech.api.capability.ILaserContainer
 import gregtech.api.metatileentity.IDataInfoProvider
 import gregtech.api.metatileentity.multiblock.IMultiblockAbilityPart
 import gregtech.common.metatileentities.multi.multiblockpart.MetaTileEntityLaserHatch
-import gregtechlite.gtlitecore.common.metatileentity.GTLiteMetaTileEntities
-import net.minecraft.creativetab.CreativeTabs
-import net.minecraft.item.ItemStack
-import net.minecraft.util.NonNullList
 import net.minecraft.util.ResourceLocation
 
 class PartMachineAdvancedLaserHatch(metaTileEntityId: ResourceLocation?, tier: Int, amperage: Int, isExport: Boolean) : MetaTileEntityLaserHatch(metaTileEntityId, isExport, tier, amperage), IMultiblockAbilityPart<ILaserContainer>, IDataInfoProvider
 {
-
-    override fun getSubItems(creativeTab: CreativeTabs, subItems: NonNullList<ItemStack>)
-    {
-        for (bus in GTLiteMetaTileEntities.LASER_INPUT_HATCH_16384)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_OUTPUT_HATCH_16384)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_INPUT_HATCH_65536)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_OUTPUT_HATCH_65536)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_INPUT_HATCH_262144)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_OUTPUT_HATCH_262144)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_INPUT_HATCH_1048576)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_OUTPUT_HATCH_1048576)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_INPUT_HATCH_4194304)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_OUTPUT_HATCH_4194304)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_INPUT_HATCH_16777216)
-        {
-            subItems.add(bus.stackForm)
-        }
-        for (bus in GTLiteMetaTileEntities.LASER_OUTPUT_HATCH_16777216)
-        {
-            subItems.add(bus.stackForm)
-        }
-    }
 
 }
--- src/main/kotlin/gregtechlite/gtlitecore/common/metatileentity/part/PartMachineAdvancedMultiFluidHatch.kt
+++ src/main/kotlin/gregtechlite/gtlitecore/common/metatileentity/part/PartMachineAdvancedMultiFluidHatch.kt
@@ -3,34 +3,10 @@ package gregtechlite.gtlitecore.common.metatileentity.part
 import gregtech.api.capability.IControllable
 import gregtech.api.metatileentity.multiblock.IMultiblockAbilityPart
 import gregtech.common.metatileentities.multi.multiblockpart.MetaTileEntityMultiFluidHatch
-import gregtechlite.gtlitecore.common.metatileentity.GTLiteMetaTileEntities
-import net.minecraft.creativetab.CreativeTabs
-import net.minecraft.item.ItemStack
-import net.minecraft.util.NonNullList
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fluids.IFluidTank
 
 class PartMachineAdvancedMultiFluidHatch(metaTileEntityId: ResourceLocation?, tier: Int, numSlots: Int, isExportHatch: Boolean) : MetaTileEntityMultiFluidHatch(metaTileEntityId, tier, numSlots, isExportHatch), IMultiblockAbilityPart<IFluidTank>, IControllable
 {
-
-    override fun getSubItems(creativeTab: CreativeTabs, subItems: NonNullList<ItemStack>)
-    {
-        for (hatch in GTLiteMetaTileEntities.QUADRUPLE_FLUID_IMPORT_HATCH)
-        {
-            subItems.add(hatch.stackForm)
-        }
-        for (hatch in GTLiteMetaTileEntities.QUADRUPLE_FLUID_EXPORT_HATCH)
-        {
-            subItems.add(hatch.stackForm)
-        }
-        for (hatch in GTLiteMetaTileEntities.NONUPLE_FLUID_IMPORT_HATCH)
-        {
-            subItems.add(hatch.stackForm)
-        }
-        for (hatch in GTLiteMetaTileEntities.NONUPLE_FLUID_EXPORT_HATCH)
-        {
-            subItems.add(hatch.stackForm)
-        }
-    }
 
 }
